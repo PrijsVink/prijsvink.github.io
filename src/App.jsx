00bb6e046ff090cd32afafc8d2a10548b77d64d2
sc0Vu <alk03073135@gmail.com>
2026-08-27 21:05:03 +0800

    Update logo / favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import brandImg from './assets/brand-logo.png'
 import './App.css'
 import ProductSearch from './components/ProductSearch';
 
@@ -10,123 +8,29 @@ function App() {
 
   return (
     <>
+      <section class="brand-intro">
+        <img
+          class="brand-logo"
+          src={brandImg}
+          alt="PrijsVink"
+        />
+
+        <h1 class="brand-name">PrijsVink</h1>
+
+        <p class="brand-tagline">
+          From every cent to every centilitre, there’s a smarter choice
+          for every shopper.
+        </p>
+      </section>
       <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>PrijsVink</h1>
-          <p>
-            Smart grocery tracking, down to the cent and centiliter.
-          </p>
-          {/* <p>
-            PrijsVink is a real-time price monitoring and canonical product normalization platform designed specifically for Dutch supermarkets.
-          </p>
-          <p>
-            By scraping live data across major Dutch grocery retailers—including Albert Heijn, Jumbo, and Dirk—PrijsVink standardizes chaotic supermarket catalog data into clean, directly comparable metrics.
-          </p> */}
-        </div>
         <div>
           <p>Let's try it out</p>
         </div>
         <div>
           <ProductSearch />
         </div>
-        {/* <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button> */}
       </section>
 
-      <div className="ticks"></div>
-
-      {/* <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section> */}
-
-      <div className="ticks"></div>
       <section id="spacer"></section>
     </>
   )
